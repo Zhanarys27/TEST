02540925978fa420c113712b179829af9958d183
ZHANARYS <nazarbakzhanarys@gmail.com>
2023-03-07 23:23:05 +0600

    ex_1 Square

---
+++ ex_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a == 0 || a == null)
+{
+    Console.WriteLine("you write not right number or don't input number");
+}
+
+else
+{
+    a*=a;
+    Console.WriteLine($"your square: {a}"); //интерполяция строк 
+    
+}
