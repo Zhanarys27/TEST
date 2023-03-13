3702c2a0563d5ebfc29c7ea52ed0c8f12668aade
ZHANARYS <nazarbakzhanarys@gmail.com>
2023-03-13 20:39:05 +0600

    add task

---
+++ Task/ex_1/Program.cs
@@ -0,0 +1,87 @@
+﻿Console.Clear();
+Console.WriteLine("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+//int b = Convert.ToInt32(Console.ReadLine());
+// if(a == 0 || a == null)
+// {
+//     Console.WriteLine("you write not right number or don't input number");
+// }
+
+// else
+// {
+//     a*=a;
+//     Console.WriteLine($"your square: {a}"); //интерполяция строк 
+
+// }
+
+
+//task 1
+// if (b*b == a)
+// {
+//     Console.WriteLine($"result: {b*b}");
+// }
+// else
+// {
+//     Console.WriteLine($"{b} не является корнем {a}");
+//     Console.WriteLine(a);
+// }
+
+// int list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+// int count = 0;
+// while(count < 7)
+// {
+//     if(a == list[])
+//     {
+
+//     }
+// }
+
+
+//task 2
+// while(a < 1 || a > 7)
+// {
+//     Console.WriteLine("wrong number, input number again: ");
+//     a = Convert.ToInt32(Console.ReadLine());
+// }
+
+// if (a == 1)
+// {
+//     Console.WriteLine("Monday");
+// }
+// else if (a == 2)
+// {
+//     Console.WriteLine("Tuesday");
+// }
+// else if (a == 3)
+// {
+//     Console.WriteLine("Wednesday");
+// }
+// else if (a == 4)
+// {
+//     Console.WriteLine("Thursday");
+// }
+// else if (a == 5)
+// {
+//     Console.WriteLine("Friday");
+// }
+// else if (a == 6)
+// {
+//     Console.WriteLine("Saturday");
+// }
+// else if (a == 7)
+// {
+//     Console.WriteLine("Sunday");
+// }
+
+
+//task 3
+// int i = -a;
+// do 
+// {
+//     Console.Write($"{i} ");
+//     i++;
+// }
+// while(i <= a);
+
+//task 4
+//Console.WriteLine(a%10);
